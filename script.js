582c7f2917c07e2a31a4cc1c944db9feb2b88eda
GiovanniMiglio <gmiglio89@gmail.com>
2025-07-31 12:42:19 +0200

    fatto il primo esercizio

---
+++ script.js
@@ -0,0 +1,12 @@
+let num1 = 1;
+console.log( num1);
+let num2 = 2;
+console.log( num2);
+let stringa = `5`;
+console.log( stringa);
+stringa = `6`
+console.log(stringa);
+const PIGREGO = 3.14159265359;
+console.log(PIGREGO);
+PIGREGO = 3
+
