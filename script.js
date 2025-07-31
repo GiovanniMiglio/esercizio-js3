933d1b6dc55984f2f03a90cbba821750401dc6af
GiovanniMiglio <gmiglio89@gmail.com>
2025-07-31 13:04:17 +0200

    esegutio esercizio2

--- script.js
+++ script.js
@@ -8,5 +8,15 @@ stringa = `6`
 console.log(stringa);
 const PIGREGO = 3.14159265359;
 console.log(PIGREGO);
-PIGREGO = 3
 
+
+// esercizio2//
+ let a = 10, b = 20, c = 30, d = 40, e = 50; 
+ let  somma =  a + b + c +d + e ;
+ let frase1 = "La somma tra i numeri equivale a";
+ console.log( frase1, ( somma));
+ let media = (a + b + c + d+ e) / 5;
+ let frase2 = "e la media equivale a";
+ console.log( frase2, (media));
+ 
+ 
